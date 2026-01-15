b0e85c2ede6f3144c142466f08ef4fb45c0dc090
Andy Allan <git@gravitystorm.co.uk>
2026-01-15 19:01:40 +0000

    Add saturatep/desaturatep

--- lib/glug/hsluv_modifiers.rb
+++ lib/glug/hsluv_modifiers.rb
@@ -19,5 +19,19 @@ module Chroma
       rgb_values = Hsluv.rgb_prepare(Hsluv.hsluv_to_rgb(h, s, new_l))
       Chroma.paint("rgb(#{rgb_values[0]}, #{rgb_values[1]}, #{rgb_values[2]})")
     end
+
+    def saturatep(amount = 10)
+      h, s, l = Hsluv.rgb_to_hsluv(rgb.r / 255.0, rgb.g / 255.0, rgb.b / 255.0)
+      new_s = [s + amount, 100].min
+      rgb_values = Hsluv.rgb_prepare(Hsluv.hsluv_to_rgb(h, new_s, l))
+      Chroma.paint("rgb(#{rgb_values[0]}, #{rgb_values[1]}, #{rgb_values[2]})")
+    end
+
+    def desaturatep(amount = 10)
+      h, s, l = Hsluv.rgb_to_hsluv(rgb.r / 255.0, rgb.g / 255.0, rgb.b / 255.0)
+      new_s = [s - amount, 0].max
+      rgb_values = Hsluv.rgb_prepare(Hsluv.hsluv_to_rgb(h, new_s, l))
+      Chroma.paint("rgb(#{rgb_values[0]}, #{rgb_values[1]}, #{rgb_values[2]})")
+    end
   end
 end
