bbdefe154740a4d740960201a9770e8360ef4556
Andy Allan <git@gravitystorm.co.uk>
2026-01-16 13:59:31 +0000

    Implement spinp

--- lib/glug/hsluv_modifiers.rb
+++ lib/glug/hsluv_modifiers.rb
@@ -40,5 +40,12 @@ module Chroma
       Chroma.paint("rgb(#{rgb_values[0]}, #{rgb_values[1]}, #{rgb_values[2]})")
     end
     alias grayscalep greyscalep
+
+    def spinp(amount = 0)
+      h, s, l = Hsluv.rgb_to_hsluv(rgb.r / 255.0, rgb.g / 255.0, rgb.b / 255.0)
+      new_h = (h + amount) % 360
+      rgb_values = Hsluv.rgb_prepare(Hsluv.hsluv_to_rgb(new_h, s, l))
+      Chroma.paint("rgb(#{rgb_values[0]}, #{rgb_values[1]}, #{rgb_values[2]})")
+    end
   end
 end
